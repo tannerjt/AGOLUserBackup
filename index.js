ca0ffa7d82ef5a41343b52d18821921a9d2fb244
tannerjt <biolsurfer85@gmail.com>
2018-08-30 11:18:10 -0700

    Use client id and secret not creds

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // AGOLUserBackup
+var getToken = require('./lib/getToken.js');
+require('dotenv').load();
+
+getToken(process.env.CLIENTID, process.env.CLIENTSECRET).then((resp) => {
+  console.log(resp)
+})
